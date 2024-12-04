6755aa6e5c287698cdbd1b5f8b4d8fe07bb27a70
Zo3265 <zohaibgul03@gmail.com>
2024-12-04 13:00:40 +0000

    Added our team folder

---
+++ GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/CC_BaseState.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseST 
+{
+    public abstract Type Entry();
+    public abstract Type Update();
+    public abstract Type Exit();
+
+
+
+
+}
---
+++ GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/CC_FSM.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CC_FSM:MonoBehaviour
+{
+
+
+    Dictionary<Type, BaseST> states; // dicitionary that will store all of the states available to the
+   // state machine with the key for a particualr state being a type associated with one of
+   // the child classes of the abstract  base class 
+    BaseST currentState; // the current state of the state machine 
+    public BaseST CState // getter and setter for current state
+    {
+        get
+        {
+            return currentState;
+        }
+        private set
+        {
+
+            currentState = value;
+        }
+
+    } 
+
+    public void setStates(Dictionary<Type, BaseST> newStates)
+    {
+        states = newStates;
+    }
+
+
+
+
+    private void Update()
+    {
+
+        if (CState == null) // if our state machine is running for the first time(we dont have a state)
+        {
+            CState = states.First().Value; // set the current state to the default sate 
+
+        }
+        else
+        {
+            var next = currentState.Update(); // continuely update the current state 
+
+            if (next != null && next.GetType() != CState.GetType()) // waiting for change in state i.e none rull return from update  
+            {
+                switchState(next); // change currentState to next state 
+            }
+        }
+
+
+    }
+
+
+
+    private void switchState(Type next)
+    {
+
+        CState.Exit();// when switching states we call the exit function for the current state before switching 
+
+        CState = states[next];//  key into the states dictionary in order to get the state being transitioned to using the type of the next state
+
+
+        CState.Entry(); // set up the state that has juts been transitioned to 
+        
+
+
+    }
+
+
+
+
+}
