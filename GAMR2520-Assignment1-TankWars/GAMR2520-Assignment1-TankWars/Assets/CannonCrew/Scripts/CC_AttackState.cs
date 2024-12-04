9b78b45905b5cb348b6020bbc1a5c5aa796fb77a
Ktos04 <kewinbinias04@gmail.com>
2024-12-04 10:24:58 +0000

    Changed some if statements in the attack state

--- GAMR2520-Assignment1-TankWars/GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/CC_AttackState.cs
+++ GAMR2520-Assignment1-TankWars/GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/CC_AttackState.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CC_AttackState : BaseST
 {
     private CC_SmartTank Tank;
+    //Game object to store enemy position
     GameObject EnemyTankPositionStore;
 
     public CC_AttackState(CC_SmartTank newTank)
@@ -19,21 +20,36 @@ public class CC_AttackState : BaseST
 
     public override Type Update()
     {
-        EnemyTankPositionStore.transform.position = Tank.enemyTank.transform.position;
 
-        if (Tank.enemyTanksFound != null)
+        if (Tank.enemyTanksFound != null) // if we see the enemy tank
         {
+            // store enemy position
+            EnemyTankPositionStore.transform.position = Tank.enemyTank.transform.position;
+            //fire at the stored position
             Tank.TurretFireAtPoint(EnemyTankPositionStore);
+            // return null since the state doesn't change, we will continue attacking
             return null;
         }
-        else if (!EnemyTankPositionStore.transform.position.Equals(Tank.enemyTank.transform.position) && !Tank.priorityManager.checkQueue(PriorityManager.queuePriority.CRITICAL, CC_SmartTank.PRIORITIES.HEALTH))
+        else if (!EnemyTankPositionStore.transform.position.Equals(Tank.enemyTank.transform.position) &&
+            Tank.priorityManager.checkQueue(PriorityManager.queuePriority.SAFE, CC_SmartTank.PRIORITIES.HEALTH) ||
+            Tank.priorityManager.checkQueue(PriorityManager.queuePriority.MINOR, CC_SmartTank.PRIORITIES.HEALTH) ||
+            Tank.priorityManager.checkQueue(PriorityManager.queuePriority.MAJOR, CC_SmartTank.PRIORITIES.HEALTH)) 
+            // else if the enemy position changed and our health is either in safe, minor or major priority
         {
+            //store the enemies last position this might not be needed though so I'll ask later
+            EnemyTankPositionStore.transform.position = Tank.enemyTank.transform.position;
+            //and chase them
             return typeof(Chase);
         }
-
-        else
+        //otherwise if our health is low, retreat
+        else if (Tank.priorityManager.checkQueue(PriorityManager.queuePriority.CRITICAL, CC_SmartTank.PRIORITIES.HEALTH))
         {
             return typeof(Retreat);
+        }
+
+        else 
+        {
+            return typeof(SearchState);
         }
     }
 
