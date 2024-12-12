76ebc503bbec75d03a1f9fd8078229125967e2a8
JRRoddy <jamesrichardroddy@hotmail.co.uk>
2024-12-12 18:39:57 +0000

    Pushing to main to get my stuff on there
    This could cause merge confilct but i have only added files nothing else was touched and ive disgarded anything else i did touch

---
+++ GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/FSMBTRBS/FSMRBSBTStates/FSMRBSBTChaseState.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CC_BTFSMRBChaseState : BaseST
+{
+
+
+    private CC_smartTankFSMRBSBT Tank;
+    GameObject EnemyTankPositionStore = new GameObject();
+    float fSpeed;
+    float tankAttackMinThresh = 20.0f;
+
+    float t = 0.0f;
+    bool hasSeenBase = false;
+    int logCounter = 0;
+    public CC_BTFSMRBChaseState(CC_smartTankFSMRBSBT newtank)
+    {
+        Tank = newtank;
+    }
+
+    public override Type Entry()
+    {
+        Debug.Log("Entered Chase " + logCounter);
+        logCounter++;
+
+        Tank.stats["chaseState"] = true;
+
+        fSpeed = 1f;
+        t = 0.0f;
+        return null;
+    }
+
+    public override Type Update()
+    {
+
+        foreach (var item in Tank.rules.GetRules) // iterates through the rules
+        {
+
+
+            if (item.CheckRule(Tank.stats) != null) // if a rule doesn't return null
+            {
+                return item.CheckRule(Tank.stats); // return the state
+            }
+        }
+
+        return null;
+
+
+
+
+    }
+
+
+
+    public override Type Exit()
+    {
+
+
+        Debug.Log("Chase Exit " + logCounter);
+        logCounter++;
+
+        Tank.stats["chaseState"] = false;
+
+        t = 0.0f;
+        fSpeed = 1f;
+        return null;
+    }
+
+
+
+
+}
---
+++ GAMR2520-Assignment1-TankWars/Assets/CannonCrew/Scripts/FSMBTRBS/FSMRBSBTStates/FSMRBSBTSearchState.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PriorityManager;
+
+public class CC_BTFSMRBSSearchState : BaseST
+{
+    // Start is called before the first frame update
+    private CC_smartTankFSMRBSBT Tank;
+    private List<GameObject> pointsOfInterest;
+    float explorationTimer;
+    GameObject behind = new GameObject();
+    bool hasFoundConsuamble = false;
+    private Type stateToReturn = null;
+    List<Vector3> priorityPositions = new List<Vector3>();
+    List<Vector3> visited;
+    GameObject priorityPosition = new GameObject();
+    Dictionary<PRIORITIES, GameObject> organisedConsumables = new Dictionary<PRIORITIES, GameObject>();
+    //private float currentSpeed = 0.85f;
+    float checkBehindWaitTime = 0.0f;
+    int logCounter = 0;
+
+    public CC_BTFSMRBSSearchState(CC_smartTankFSMRBSBT tank){
+        
+       this.Tank = tank;
+    }
+
+    public override Type Entry()
+    {
+        //  currentSpeed = 0.85f;
+        priorityPosition = new GameObject();
+        stateToReturn = null;
+        Debug.Log("Entered Search " + logCounter);
+        Tank.resetTimersIntoSearch(); // reset timers coming in from search
+        Tank.stats["searchState"] = true; // search state true
+
+        logCounter++;
+        return null;
+    }
+
+    public override Type Update()
+    {
+
+
+        foreach (var item in Tank.rules.GetRules) // iterates through the rules
+        {
+
+
+            if (item.CheckRule(Tank.stats) != null) // if a rule doesn't return null
+            {
+                return item.CheckRule(Tank.stats); // return the state
+            }
+        }
+
+        return null;
+
+
+
+
+    }
+    public override Type Exit()
+    {
+        Debug.Log("Search Exit " + logCounter);
+
+        Tank.stats["searchState"] = false;
+        hasFoundConsuamble = false;
+        logCounter++;
+        stateToReturn = null;
+        priorityPositions.Clear();
+        organisedConsumables.Clear();
+        //  currentSpeed = 0.85f;
+        explorationTimer = 0.0f;
+        priorityPosition = new GameObject();
+
+        return null;
+    }
+}
